29b37a7fbbd7f8ac5230956e52c4a22eaa42af89
sahin.hanay <sahin.hanay@mobillium.com>
2019-10-05 23:23:23 +0300

    galleries image

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -172,7 +172,7 @@ class UserController extends Controller
         }
 
         $model = Like::query()
-            ->with('user.image', 'festival.galleries')
+            ->with('user.image', 'festival.galleries.image')
             ->where('user_id', $userId)
             ->get();
 
@@ -192,7 +192,7 @@ class UserController extends Controller
         }
 
         $model = Comment::query()
-            ->with('user.image', 'festival.galleries')
+            ->with('user.image', 'festival.galleries.image')
             ->where('user_id', $userId)
             ->get();
 

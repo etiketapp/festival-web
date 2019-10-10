60990b81360092ca44b4afd242214c469e3338ff
-add <sahin.hanay@mobillium.com>
2019-10-11 01:16:07 +0300

    latest message

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -25,7 +25,7 @@ class Conversation extends Model
     public function getLatestMessageAttribute()
     {
         if(Auth::user()) {
-            return $this->messages()->first();
+            return $this->messages->first();
         }
 
         return false;
@@ -36,7 +36,7 @@ class Conversation extends Model
      */
     public function messages()
     {
-        return $this->hasMany(Message::class)->orderBy('date', 'asc');
+        return $this->hasMany(Message::class)->orderBy('id', 'desc');
     }
 
     /**

aace4d41e0c610eae9edc3c3029bec81049df665
tunay <sahin.hanay@mobillium.com>
2019-10-14 22:59:23 +0300

    unread messages

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -235,7 +235,7 @@ class UserController extends Controller
 
         foreach ($user->conversations as $cv)
         {
-            $conversations->push($cv->unread_message);
+            $conversations->push($cv->unread_messages);
         }
 
         $model['unread_message_count'] = $conversations->count();

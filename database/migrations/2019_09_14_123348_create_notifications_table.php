590f118cb8e9e397e6fb23c6c3474dbf7d438ae7
sahin <sahin.hanay@mobillium.com>
2019-10-30 20:49:15 +0300

    Notifications channel added

--- database/migrations/2019_09_14_123348_create_notifications_table.php
+++ database/migrations/2019_09_14_123348_create_notifications_table.php
@@ -23,7 +23,7 @@ class CreateNotificationsTable extends Migration
 
             $table->unsignedInteger('user_id')->nullable();
             $table->foreign('user_id')->references('id')
-                ->on('userss')->onUpdate('cascade')->onDelete('cascade');
+                ->on('users')->onUpdate('cascade')->onDelete('cascade');
 
             $table->unsignedInteger('draw_id')->nullable();
             $table->foreign('draw_id')->references('id')
